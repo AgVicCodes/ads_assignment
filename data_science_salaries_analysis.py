b17cd73caa6b25016db94bd89b2a2bde75737117
AgVicCodes <victoragbeleye2@gmail.com>
2024-02-26 01:22:40 +0000

    Adjusted the path to file in the jupyter file, Grouped DF by multiple columns in the py file Commented out unnecessary stuff in the test file and Edited the tab to indent settings on the settings file

--- data_science_salaries_analysis.py
+++ data_science_salaries_analysis.py
@@ -1,8 +1,60 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-x = np.array([2, 3, 4, 5])
+sal = pd.read_csv('csv/salaries.csv', index_col = 0)
 
-salaries = pd.read_csv('salaries.csv', index_col = 0)
+def clean_dataframe(df):
+    """
+        Function that takes a DataFrame, 
+        cleans and prepare it for 
+        further analyis.
+    """
+    # Dropping duplicate rows
+    df = df.drop_duplicates()
+    
+    # Removing null values
+    for i in df.columns:
+        if df[i].isna().all():
+            df.drop_na()  
+    
+    return df
 
-print(salaries)
+sal.reset_index(drop = False, inplace = True)
+sal = clean_dataframe(sal)
+
+# print(sal)
+# print(sal.describe(include="number"))
+
+
+# Pie Chart
+plt.figure(dpi = 144)
+
+sal_by_year = sal.groupby(["work_year"])['salary_in_usd']
+
+# print()
+
+plt.pie(sal_by_year)
+plt.legend(loc = "upper right")
+plt.show()
+
+# # Bar Chart
+# plt.figure(dpi = 144)
+
+# plt.bar(sal.salary)
+# plt.legend(loc = "upper right")
+# plt.show()
+
+# # Histogram Chart
+# plt.figure(dpi = 144)
+
+# plt.hist(sal.salary)
+# plt.legend(loc = "upper right")
+# plt.show()
+
+# # Scatter Chart
+# plt.figure(dpi = 144)
+
+# plt.scatter(sal.salary)
+# plt.legend(loc = "upper right")
+# plt.show()
---
+++ pltpractice.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(2, 2, figsize = (5, 3), layout = "constrained")
+
+
+
+## Creating figures
+# fig, ax = plt.subplots(2, 2, figsize = (4, 3), layout = 'constrained')
+# fig, ax = plt.subplot_mosaic([['left', 'A'], ['left', 'B']], figsize = (4, 3), layout = 'constrained')
+
+# x = np.array(np.linspace(0, 2, 25))
+# y = x ** 3
+
+# plt.plot(x, y, 'b-')
+# plt.show()
